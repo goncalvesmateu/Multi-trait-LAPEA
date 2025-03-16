0e56dee41936a55c6fec6ae9c2a6230218fa715d
Mateus Goncalves <mateus.tvgoncalves@gmail.com>
2025-03-15 21:02:12 -0300

    Updated

--- R/scriptChapter5book.R
+++ R/scriptChapter5book.R
@@ -1,4 +1,11 @@
 ################################################################################
+#
+# Script of chapter 5
+#
+# Multivariate Statistical machine Learning methods for genomic prediction
+#
+# by Osval Antonio Montesinos López, Abelardo Montesinos López & Jose Crossa 
+#
 ################################################################################
 # Installing packages
 # Github version
@@ -8,4 +15,8 @@ install_github('covaruber/sommer')
 # CRAN version
 install.packages('sommer',dependencies = TRUE)
 library(sommer)
+################################################################################
+# GBLUP model
+A = mmer(y ~ 1,random= ~ vs(GID,Gu=G) , rcov= ~ vs(units),
+         data=dat_F, verbose=FALSE)
 ################################################################################
