73df2ed8f03b8af550102bc9fe9db9e5542a06aa
Mateus Goncalves <mateus.tvgoncalves@gmail.com>
2025-03-17 13:43:31 -0300

    Updated

--- R/scriptShortCourseMultitraitLAPEA.R
+++ R/scriptShortCourseMultitraitLAPEA.R
@@ -91,46 +91,6 @@ h2a <- (mod.1$sigma$`u:id`) /(mod.1$sigma$`u:id`+mod.1$sigma$units); h2a # narro
 vpredict(mod.1, h2 ~ (V1+V2) / ( V1+V2+V3) ) # broad-sense function
 vpredict(mod.1, h2 ~ (V1) / ( V1+V3) ) # narrow sense function
 ######################################################################################
-# NOT WORKING NOT WORKING NOT WORKING NOT WORKING
-# Cross-validation Single-trait models with no marker information using sommer for Yield
-# Indentity matrix 
-I <- diag(nrow(data.1.2))
-colnames(I) <- rownames(I) <- rownames(DT)
-#30 Replicates
-rep <- 3
-#5 random partitions
-fold <- 5
-n <- dim(data.1.2)[1]
-Replicates <- data.frame(Replicate = 1:rep,MSEP = NA,Cor = NA) 
-for (j in 1:rep){ 
-  set.seed(j)
-  Partitions <- replicate(fold,sample(n,0.20*n,replace = F))
-  Table <- data.frame(Partitions = 1:fold,MSEP = NA,Cor = NA)
-  for(i in 1:fold) {
-    tst <- Partitions[,i]
-    data.1.2$y_NA <- data.1.2$Yield
-    data.1.2$y_NA[tst] <- NA
-    #Fit model
-    fit = mmer(y_NA~ 1,
-               random=~ vsr(id, Gu=I, Gtc=unsm(2)), # unstructured model (each pair of random effects or residuals to have its own distinct covariance value)
-               rcov=~units,
-               data=data.1.2,verbose = FALSE)
-    #BLUPs
-    bv <- fit$U$`u:id`$y_NA
-    #Prediction of testing
-    yp_tst <- bv[tst]
-    #MSEP and Cor
-    Table$MSEP[i] <- mean((data.1.2$Yield[tst]-yp_tst)^2)
-    Table$Cor[i] <- cor(data.1.2$Yield[tst],yp_tst)
-  }
-  Replicates$MSEP[j] <- mean((na.omit(Table$MSEP))) # mean of folds
-  Replicates$Cor[j] <-mean((na.omit(Table$Cor))) # mean of folds
-}
-
-mean(Replicates$MSEP) # mean of replicates
-mean(Replicates$Cor) # mean of replicates
-######################################################################################
-######################################################################################
 # Cross-validation Single-trait models (GBLUP) using sommer for Yield
 #30 Replicates
 rep <- 3
@@ -214,5 +174,59 @@ fit <- mmer(cbind(Yield,FruitAver)~ 1,
 # Model summary
 summary(fit)$varcomp
 fit$sigma$`u:id`
+################################################################################
+# Cross-validation Single-trait models (GBLUP) using sommer for Yield and FruitAver
+#30 Replicates
+#30 Replicates
+rep <- 3
+#5 random partitions
+fold <- 5
+n <- dim(data.1.2)[1]
+Replicates <- data.frame(Replicate = 1:rep,MSEP_T1 = NA,Cor_T1 = NA,
+                         MSEP_T2 = NA,Cor_T2 = NA) 
 
+for (j in 1:rep){ 
+  set.seed(j)
+  Partitions <- replicate(fold,sample(n,0.20*n,replace = F))
+  Table <- data.frame(Partitions = 1:fold,MSEP_T1 = NA,Cor_T1 = NA,
+                      MSEP_T2 = NA,Cor_T2 = NA)
+  for(i in 1:fold) {
+    tst <- Partitions[,i]
+    data.1.2$y_NA.1 <- data.1.2$Yield
+    data.1.2$y_NA.1[tst] <- NA
+    data.1.2$y_NA.2 <- data.1.2$FruitAver
+    data.1.2$y_NA.2[tst] <- NA
+    #Fit model
+    fit <- mmer(cbind(y_NA.1,y_NA.2)~ 1,
+                random= ~ vsr(id, Gu=G,Gtc=unsm(2)),
+                rcov= ~ vsr(units,Gtc=diag(2)), data=data.1.2,
+                verbose=F, method = "AI")
+    #BLUPs
+    b_ls <- fit$U$`u:id`
+    b_T1 <- b_ls$y_NA.1 # Trait 1
+    b_T2 <- b_ls$y_NA.2 # Trait 2
+    b_mat <- cbind(b_T1,b_T2)
+    #Prediction of testing for both traits
+    yp_tst = b_mat[tst,1] # Trait 1
+    y2p_tst = b_mat[tst,2] # Trait 2
+    #MSEP and Cor
+    #Trait 1
+    Table$MSEP_T1[i] <- mean((data.1.2$Yield[tst]-yp_tst)^2)
+    Table$Cor_T1[i] <- cor(data.1.2$Yield[tst],yp_tst)
+    #Trait 2
+    Table$MSEP_T2[i] <- mean((data.1.2$FruitAver[tst]-y2p_tst)^2)
+    Table$Cor_T2[i] <- cor(data.1.2$FruitAver[tst],y2p_tst)
+  }
+  Replicates$MSEP_T1[j] <- mean(na.omit(Table$MSEP_T1))
+  Replicates$Cor_T1[j] <-mean(na.omit(Table$Cor_T1))
+  Replicates$MSEP_T2[j] <- mean(na.omit(Table$MSEP_T2))
+  Replicates$Cor_T2[j] <-mean(na.omit(Table$Cor_T2))
+}
+
+mean(Replicates$MSEP_T1)
+mean(Replicates$Cor_T1)
+mean(Replicates$MSEP_T2)
+mean(Replicates$Cor_T2)
+################################################################################
+#################################### END #######################################
 ################################################################################
